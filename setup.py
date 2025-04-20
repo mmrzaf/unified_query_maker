ba8e89b6c644cfaaa53131a3eff2e2c029de6332
m.farkhondeh <pheri@paya.bigdata.net>
2025-04-20 15:09:05 +0330

    fix typo to release 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="unified_query_maker",
-    version="0.1.0",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=["jsonschema"],
     author="Pheri",
--- unified_query_maker/translators/mssql_translator.py
+++ unified_query_maker/translators/mssql_translator.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 from .base_sql import SQLTranslator
 
 
