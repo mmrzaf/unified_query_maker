d96ce2f75b0f9c8f734b443f798acdb7ff754e1c
mmrzaf <pherry@gmail.com>
2025-11-23 17:19:20 +0330

    chore(exports): Update package __init__ to export new Pydantic models and finalize type hints

--- unified_query_maker/__init__.py
+++ unified_query_maker/__init__.py
@@ -1,5 +1,8 @@
 from unified_query_maker.validators.schema_validator import validate_uql_schema
 from unified_query_maker.validators.semantic_validator import validate_uql_semantics
+
+from unified_query_maker.models import UQLQuery, OrderByItem, WhereClause
+
 from unified_query_maker.translators.mysql_translator import MySQLTranslator
 from unified_query_maker.translators.elasticsearch_translator import (
     ElasticsearchTranslator,
@@ -20,8 +23,16 @@ from unified_query_maker.translators.mongodb_translator import MongoDBTranslator
 from unified_query_maker.translators.postgresql_translator import PostgreSQLTranslator
 
 __all__ = [
+    # Validation
     "validate_uql_schema",
     "validate_uql_semantics",
+
+    # Models
+    "UQLQuery",
+    "OrderByItem",
+    "WhereClause",
+
+    # Translators
     "MySQLTranslator",
     "ElasticsearchTranslator",
     "Elasticsearch7Translator",
